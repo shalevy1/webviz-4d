acde6cf1bf23b796cf238c6f06265e7b26d58daf
ashildskalnes <ashska@equinor.com>
2020-07-08 12:12:02 +0200

    Unit test update

--- webviz_4d/tests/data/unit_tests/data_input/test_config.py
+++ webviz_4d/tests/data/unit_tests/data_input/test_config.py
@@ -3,10 +3,10 @@ from webviz_4d._datainput.common import read_config, get_config_item
 with open("../../fmu_directory.txt", "r") as f:
     FMU_DIRECTORY = f.read()
 
+
 def test_get_config_item():
     config_file = "../../reek_4d.yaml"
     config = read_config(config_file)
-    print(get_config_item(config,"fmu_directory"))
-    
-    assert get_config_item(config,"fmu_directory") == FMU_DIRECTORY
-    
+    print(get_config_item(config, "fmu_directory"))
+
+    assert get_config_item(config, "fmu_directory") == FMU_DIRECTORY
